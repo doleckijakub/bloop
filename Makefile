1eda627441edc1f719178daa031619c9c8fe46ea
Jakub Dolecki <doleckijakub@users.noreply.github.com>
2025-05-07 23:10:30 +0200

    Starting to do stuff

--- Makefile
+++ Makefile
@@ -1,16 +1,16 @@
 all: bloop
 
-bloop: src/*.go src/data/million.txt
+bloop: src/*.go
 	go build -o $@ ./src
 
-src/data/million.txt: src/data/majestic_million.csv
+data/million.txt: data/majestic_million.csv
 	cat $< | tail -n 1000000 | tr , ' ' | awk '{print $$3}' > $@
 
-src/data/majestic_million.csv: src/data
+data/majestic_million.csv: data
 	@if [ ! -f "$@" ]; then \
 		echo "Downloading majestic_million.csv..."; \
 		wget -O "$@" https://downloads.majestic.com/majestic_million.csv; \
 	fi
 
-src/data:
+data:
 	mkdir -p $@
--- src/main.go
+++ src/main.go
@@ -1,55 +1,173 @@
 package main
 
 import (
-	_ "embed"
-	"strings"
+	// "context"
 	"fmt"
+	// "io"
+	"log"
+	// "net/http"
 	"os"
-	"net/http"
+	// "strings"
+	// "time"
+
+	"github.com/joho/godotenv"
+	// "github.com/jackc/pgx/v5/pgxpool"
 )
 
-//go:embed data/million.txt
-var million []byte
+//
+// SCRAPER
+//
 
-func scrape() {
-	fmt.Println("scraping...\n")
-	for _, domain := range strings.Split(strings.TrimRight(string(million), "\n"), "\n") {
-		fmt.Println(domain)
-	}
+func can_scrape_domain(domain *string) {
+	// urls := []string{
+	// 	fmt.Sprintf("https://%s/robots.txt", domain),
+	// 	fmt.Sprintf("http://%s/robots.txt", domain),
+	// }
+
+	// var resp *http.Response
+	// var err error
+	// var finalURL string
+
+	// for _, url := range urls {
+	// 	resp, err = client.Get(url)
+	// 	if err == nil {
+	// 		finalURL = url
+	// 		break
+	// 	}
+	// }
+
+	// if err != nil {
+	// 	fmt.Printf("!!! [%s] Failed to fetch robots.txt: %v\n", domain, err)
+	// 	continue
+	// }
+	// defer resp.Body.Close()
+
+	// body, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	fmt.Printf("!!! [%s] Failed to read response: %v\n", domain, err)
+	// 	continue
+	// }
+
+	// robotsTxt := string(body)
+	// scrapingAllowed := true
+
+	// lines := strings.Split(robotsTxt, "\n")
+	// for i := 0; i < len(lines); i++ {
+	// 	line := strings.TrimSpace(lines[i])
+	// 	if strings.HasPrefix(line, "User-agent:") {
+	// 		agent := strings.TrimSpace(strings.TrimPrefix(line, "User-agent:"))
+	// 		if agent == "*" {
+	// 			for j := i + 1; j < len(lines); j++ {
+	// 				nextLine := strings.TrimSpace(lines[j])
+	// 				if strings.HasPrefix(nextLine, "User-agent:") {
+	// 					break
+	// 				}
+	// 				if strings.HasPrefix(nextLine, "Disallow:") {
+	// 					path := strings.TrimSpace(strings.TrimPrefix(nextLine, "Disallow:"))
+	// 					if path == "/" {
+	// 						scrapingAllowed = false
+	// 						break
+	// 					}
+	// 				}
+	// 			}
+	// 			break
+	// 		}
+	// 	}
+	// }
+
+	// return scrapingAllowed
 }
 
-func root_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "req: %s\n", r.URL.Path)
+func scrape_domain(domain *string) {
+	fmt.Printf("scraping %s\n", *domain)
 }
 
-func serve(port string) {
-	address := ":" + port
+func scrape_from_file(filename *string) {
+	fmt.Printf("scraping from %s\n", *filename)
 
-	http.HandleFunc("/", root_handler)
-	fmt.Printf("Starting server on %s\n", address)
-	err := http.ListenAndServe(address, nil)
-	if err != nil {
-		fmt.Println("Error starting server: ", err)
-	}
+	// client := &http.Client{
+	// 	Timeout: 5 * time.Second,
+	// }
+
+	// for _, domain := range strings.Split(strings.TrimRight(string(million), "\n"), "\n") {
+	// 	if domain == "" {
+	// 		continue
+	// 	}
+
+	// 	scrape_domain(&domain)
+	// }
+}
+
+//
+// SERVER
+//
+
+// func root_handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "req: %s\n", r.URL.Path)
+// }
+
+// func serve(port string) {
+// 	address := ":" + port
+
+// 	http.HandleFunc("/", root_handler)
+// 	fmt.Printf("Starting server on %s\n", address)
+// 	err := http.ListenAndServe(address, nil)
+// 	if err != nil {
+// 		fmt.Println("Error starting server: ", err)
+// 	}
+// }
+
+//
+// MAIN
+//
+
+func usage(code int) {
+	fmt.Printf("Usage: %s -h,--help\n", os.Args[0])
+	fmt.Printf("       %s scrape <website>\n", os.Args[0])
+	fmt.Printf("       %s scrape -f <website list file>\n", os.Args[0])
+	// fmt.Printf("       %s serve [port]\n", os.Args[0])
+	os.Exit(code)
 }
 
 func main() {
+	// init
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// parse args
+
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: %s scrape")
-		fmt.Println("       %s serve [port]")
-		return
+		usage(1)
 	}
 
 	switch os.Args[1] {
+	case "-h", "--help":
+		usage(0)
 	case "scrape":
-		scrape()
-	case "serve":
-		port := "8080"
-
-		if len(os.Args) == 3 {
-			port = os.Args[2]
+		if len(os.Args) < 3 {
+			usage(1)
 		}
 
-		serve(port)
+		switch os.Args[2] {
+		case "-f":
+			if len(os.Args) < 4 {
+				usage(1)
+			}
+
+			scrape_from_file(&os.Args[3])
+		default:
+			scrape_domain(&os.Args[2])
+		}
+	// case "serve":
+	// 	port := "8080"
+
+	// 	if len(os.Args) < 3 {
+	// 		port = os.Args[2]
+	// 	}
+
+	// 	serve(port)
 	}
 }
